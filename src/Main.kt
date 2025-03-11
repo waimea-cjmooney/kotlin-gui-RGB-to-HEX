1300947158029ea82a6d742393cc4251c3cb4670
waimea-cjmooney <cjmooney@waimea.school.nz>
2025-03-12 12:25:21 +1300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -12,6 +12,8 @@ import com.formdev.flatlaf.FlatDarkLaf
 import java.awt.*
 import java.awt.event.*
 import javax.swing.*
+import javax.swing.event.ChangeEvent
+import javax.swing.event.ChangeListener
 
 
 /**
@@ -28,24 +30,29 @@ fun main() {
  * Defines the UI and responds to events
  * The app model should be passwd as an argument
  */
-class MainWindow : JFrame(), ActionListener, KeyListener {
+class MainWindow : JFrame(), ActionListener, KeyListener, ChangeListener, MouseListener {
 
+    private var timer: Timer
+    private var whatClicked: String = ""
+    private var clicked: Boolean = false
     // Fields to hold the UI elements
     private lateinit var redLabel: JLabel
     private lateinit var redMinus: JButton
     private lateinit var redPlus: JButton
     private lateinit var redField: JTextField
+    private lateinit var redSlider: JSlider
 
     private lateinit var greenLabel: JLabel
     private lateinit var greenMinus: JButton
     private lateinit var greenPlus: JButton
     private lateinit var greenField: JTextField
+    private lateinit var greenSlider: JSlider
 
     private lateinit var blueLabel: JLabel
     private lateinit var blueMinus: JButton
     private lateinit var bluePlus: JButton
     private lateinit var blueField: JTextField
-
+    private lateinit var blueSlider: JSlider
 
     private lateinit var hexTitle: JLabel
     private lateinit var hexDisplay: JLabel
@@ -60,6 +67,9 @@ class MainWindow : JFrame(), ActionListener, KeyListener {
 
         setLocationRelativeTo(null)     // Centre the window
         isVisible = true                // Make it visible
+
+        timer=  Timer(100, this)
+        timer.start()
     }
 
     /**
@@ -106,6 +116,13 @@ class MainWindow : JFrame(), ActionListener, KeyListener {
         redField.addKeyListener(this)
         add(redField)
 
+        redSlider = JSlider()
+        redSlider.bounds = Rectangle(100, 100, 150, 50)
+        redSlider.minimum = 0
+        redSlider.maximum = 255
+        redSlider.addChangeListener(this)
+        add(redSlider)
+
 
         greenLabel = JLabel("G")
         greenLabel.horizontalAlignment = SwingConstants.CENTER
@@ -131,6 +148,13 @@ class MainWindow : JFrame(), ActionListener, KeyListener {
         greenField.addActionListener(this)
         greenField.addKeyListener(this)
         add(greenField)
+
+        greenSlider = JSlider()
+        greenSlider.bounds = Rectangle(100, 200, 150, 50)
+        greenSlider.minimum = 0
+        greenSlider.maximum = 255
+        greenSlider.addChangeListener(this)
+        add(greenSlider)
 
 
 
@@ -159,6 +183,13 @@ class MainWindow : JFrame(), ActionListener, KeyListener {
         blueField.addKeyListener(this)
         add(blueField)
 
+        blueSlider = JSlider()
+        blueSlider.bounds = Rectangle(100, 300, 150, 50)
+        blueSlider.minimum = 0
+        blueSlider.maximum = 255
+        blueSlider.addChangeListener(this)
+        add(blueSlider)
+
 
         hexTitle = JLabel("HEX")
         hexTitle.horizontalAlignment = SwingConstants.CENTER
@@ -184,21 +215,6 @@ class MainWindow : JFrame(), ActionListener, KeyListener {
     }
 
 
-    /**
-     * Handle any UI events (e.g. button clicks)
-     */
-    override fun actionPerformed(e: ActionEvent?) {
-        when (e?.source) {
-            redMinus   -> redField.text   = (redField.text.toInt()   - 1).toString()
-            redPlus    -> redField.text   = (redField.text.toInt()   + 1).toString()
-            greenMinus -> greenField.text = (greenField.text.toInt() - 1).toString()
-            greenPlus  -> greenField.text = (greenField.text.toInt() + 1).toString()
-            blueMinus  -> blueField.text  = (blueField.text.toInt()  - 1).toString()
-            bluePlus   -> blueField.text  = (blueField.text.toInt()  + 1).toString()
-        }
-        updateUi()
-    }
-
     private fun updateUi() {
         if (redField.text.toInt() > 255) redField.text = "0"
         if (greenField.text.toInt() > 255) greenField.text = "0"
@@ -207,6 +223,10 @@ class MainWindow : JFrame(), ActionListener, KeyListener {
         if (greenField.text.toInt() < 0) greenField.text = "255"
         if (blueField.text.toInt() < 0) blueField.text = "255"
 
+        redSlider.value = redField.text.toInt()
+        greenSlider.value = greenField.text.toInt()
+        blueSlider.value = blueField.text.toInt()
+
         val rgb =  Color(redField.text.toInt(), greenField.text.toInt(), blueField.text.toInt())
         hexDisplay.background = rgb
         hexCodeLabel.text = String.format("#%02x%02x%02x", redField.text.toInt(), greenField.text.toInt(), blueField.text.toInt())
@@ -214,6 +234,53 @@ class MainWindow : JFrame(), ActionListener, KeyListener {
         greenField.background = Color(0, greenField.text.toInt(), 0)
         blueField.background = Color(0, 0, blueField.text.toInt())
     }
+
+    /**
+     * Handle any UI events (e.g. button clicks)
+     */
+    override fun actionPerformed(e: ActionEvent?) {
+
+        when (e?.source) {
+            redMinus   -> {
+                whatClicked     = "redMinus"
+                redField.text   = (redField.text.toInt()   - 1).toString()
+            }
+            redPlus    -> {
+                whatClicked     = "redPlus"
+                redField.text   = (redField.text.toInt()   + 1).toString()
+            }
+            greenMinus -> {
+                whatClicked     = "greenMinus"
+                greenField.text = (greenField.text.toInt() - 1).toString()
+            }
+            greenPlus  -> {
+                whatClicked     = "greenPlus"
+                greenField.text = (greenField.text.toInt() + 1).toString()
+            }
+            blueMinus  -> {
+                whatClicked     = "blueMinus"
+                blueField.text  = (blueField.text.toInt()  - 1).toString()
+            }
+            bluePlus   -> {
+                whatClicked     = "bluePlus"
+                blueField.text  = (blueField.text.toInt()  + 1).toString()
+            }
+
+            timer      -> {
+                when (whatClicked) {
+                    "redMinus"   -> {if (clicked) redField.text.toInt()   - 1}
+                    "redPlus"    -> {if (clicked) redField.text.toInt()   + 1}
+                    "greenMinus" -> {if (clicked) greenField.text.toInt() - 1}
+                    "greenPlus"  -> {if (clicked) greenField.text.toInt() + 1}
+                    "blueMinus"  -> {if (clicked) blueField.text.toInt()  - 1}
+                    "bluePlus"   -> {if (clicked) blueField.text.toInt()  + 1}
+                    else -> println("tick")
+                }
+            }
+        }
+        updateUi()
+    }
+
 
     override fun keyTyped(e: KeyEvent?) {
         println("Key typed")
@@ -228,6 +295,38 @@ class MainWindow : JFrame(), ActionListener, KeyListener {
         println("Key Released")
         updateUi()
     }
+
+    override fun stateChanged(e: ChangeEvent?) {
+        when (e?.source) {
+            redSlider   -> redField.text   = redSlider.value.toString()
+            greenSlider -> greenField.text = greenSlider.value.toString()
+            blueSlider  -> blueField.text  = blueSlider.value.toString()
+        }
+        updateUi()
+    }
+
+    override fun mouseClicked(e: MouseEvent?) {
+        println("Mouse clicked")
+    }
+
+    override fun mousePressed(e: MouseEvent?) {
+        clicked = true
+        println("mousePressed")
+    }
+
+    override fun mouseReleased(e: MouseEvent?) {
+        clicked = false
+        println("mouseReleased")
+    }
+
+    override fun mouseEntered(e: MouseEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun mouseExited(e: MouseEvent?) {
+        TODO("Not yet implemented")
+    }
+
 
 }
 
